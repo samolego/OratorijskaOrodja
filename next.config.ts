d24cba6765b7ef3ddcbdeffdfe22a0639e992f62
samolego <34912839+samolego@users.noreply.github.com>
2025-03-24 19:01:43 +0100

    Workflow fixing

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+/* This file will get overwritten by the build process on Github pages! */
 const nextConfig: NextConfig = {
   /* config options here */
 };
