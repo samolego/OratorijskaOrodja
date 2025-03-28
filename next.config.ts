676e67cd4007206128b8643e52b9bb9cb8921ede
samolego <34912839+samolego@users.noreply.github.com>
2025-03-28 09:30:28 +0100

    Use subdomain

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 /* This file will get overwritten by the build process on Github pages! */
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/OratorijskaOrodja",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
